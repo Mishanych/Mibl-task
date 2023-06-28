40cfccdee01b839b59e80dcb516156f8a9eae1d4
Mishanych <myhaylo03@gmail.com>
2023-06-29 00:44:51 +0300

    added scene management; added main menu

---
+++ Assets/_Project/SceneManagement/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using UnityEngine.SceneManagement;
+
+namespace SceneManagement
+{
+    public class SceneLoader : ISceneLoaderService
+    {
+        private const int IndexOfNonExistentScene = -1;
+        private const string LoadingScreenScene = "LoadingScreen";
+
+        private int _activeSceneIndex = 0;
+
+        public void EnableLoadingScreen()
+        {
+            SceneManager.LoadScene(LoadingScreenScene);
+        }
+
+        public void EnableSceneAfterLoading()
+        {
+            string pathToScene = SceneUtility.GetScenePathByBuildIndex(_activeSceneIndex);
+            string sceneName = Path.GetFileNameWithoutExtension(pathToScene);
+
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public void SwitchSceneForward()
+        {
+            _activeSceneIndex++;
+        }
+
+        public void SwitchSceneBack()
+        {
+            _activeSceneIndex--;
+        }
+    }
+}
