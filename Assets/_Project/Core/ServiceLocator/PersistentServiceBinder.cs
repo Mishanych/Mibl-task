fe9f04369c0411afe80cc7e5016a6429bfa118ce
Mishanych <73424778+Mishanych@users.noreply.github.com>
2023-06-28 23:42:26 +0300

    Added loading screen (#1)
    * added ServiceLocator
    
    * added loading screen; added TextMeshPro

---
+++ Assets/_Project/Core/ServiceLocator/PersistentServiceBinder.cs
@@ -0,0 +1,17 @@
+﻿using Core.ServiceLocator;
+using LoadingScreenScene;
+using UnityEngine;
+
+namespace Core.Persistent
+{
+    public static class PersistentServiceBinder
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void RegisterServices()
+        {
+            Service serviceInstance = Service.Instance;
+
+            serviceInstance.Register<ILoadingScreenService>(new LoadingScreen());
+        }
+    }
+}
