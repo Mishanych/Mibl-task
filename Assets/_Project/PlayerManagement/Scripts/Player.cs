c43aa5574f1e554a911c9419c996bb79bc799ece
Mishanych <myhaylo03@gmail.com>
2023-07-01 08:08:39 +0300

    fixed bugs

--- Assets/_Project/PlayerManagement/Scripts/Player.cs
+++ Assets/_Project/PlayerManagement/Scripts/Player.cs
@@ -7,6 +7,7 @@ namespace PlayerManagement
     {
         private const string HorizontalInputKeyWord = "Horizontal";
         private const string VerticalInputKeyWord = "Vertical";
+        private const int SpeedMultiplier = 20;
 
         private float _horizontalInput;
         private float _verticalInput;
@@ -27,14 +28,36 @@ namespace PlayerManagement
 
         public void GetInput()
         {
+            #if UNITY_EDITOR
             _horizontalInput = Input.GetAxis(HorizontalInputKeyWord);
             _verticalInput = Input.GetAxis(VerticalInputKeyWord);
+            #endif
+
         }
 
         public void Move(Transform player, PlayerSettingsHolder playerSettingsHolder)
         {
+#if UNITY_EDITOR
             Vector3 direction = new Vector3(_horizontalInput, 0f, _verticalInput);
-            player.Translate(direction * playerSettingsHolder.WalkSettings.Speed * Time.deltaTime);
+            player.Translate(direction * playerSettingsHolder.WalkSettings.Speed * SpeedMultiplier * Time.deltaTime);
+#endif
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    Vector3 direction2 = new Vector3(
+                        touch.deltaPosition.x,
+                        0f,
+                        touch.deltaPosition.y);
+
+                    player.Translate(direction2 * playerSettingsHolder.WalkSettings.Speed * Time.deltaTime);
+                }
+            }
+#endif
         }
 
         public void Kill()
